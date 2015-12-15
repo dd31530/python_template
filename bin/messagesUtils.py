5330437107cb80bbef9068962b361d8bf02ccbfd
ddelanno <daniel.delannoy@sogeti.com>
2015-12-15 11:10:04 +0100

    add a python template project

---
+++ bin/messagesUtils.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
++----------------------------------------------------------------------------+
+| messagesUtils:  toolbox for displaying formated messages                   |
++---------+----------+----------+--------------------------------------------+
+| version | datecode | author   | history                                    |
++---------+----------+----------+--------------------------------------------+
+| 1.0     | 151214   | DD       | creation                                   |
++---------+----------+----------+--------------------------------------------+
+'''
+
+VERSION = 'v1.0_151214'
+
+################################################################################
+# external modules
+################################################################################
+
+import sys
+import inspect
+import ast
+import datetime
+
+
+##############################################################################
+# functions 
+##############################################################################
+
+def header():
+   '''
+   displays current module docstring
+   '''
+   f=inspect.stack()[1][1]
+   m=ast.parse(''.join(open(f)))
+   print "\n%s" % ast.get_docstring(m)
+
+
+def banner(mess):
+   '''
+   displays banner message
+   '''
+   sep = '\n+'+76*'-'+'+\n'
+   mess = '| ' +mess + (75-len(mess))*' ' + '|'
+   print sep, mess, sep
+
+
+def footer(code=0):
+   '''
+   displays current module execution ending
+   '''
+   banner('end of ' + sys.argv[0])
+   sys.exit(code)
+
+
+def whosdaddy():
+   '''
+   return parent function name
+   '''
+   return inspect.stack()[3][3]
+
+
+def whoami():
+   '''
+   return current function name
+   '''
+   whoami=whosdaddy()
+   if whoami=='<module>': whoami='module'
+   return whoami
+
+
+def message(mess, severity='info '):
+   '''
+   print a formated message: "[severity] function name> mess"
+      default severity value is 'info'
+      for 'debug' severity, message will be display only if DEBUG=True
+   '''
+   if severity=='debug' and not DEBUG: return
+   if TIMESTAMP:
+     ts=datetime.datetime.strftime(datetime.datetime.now(), '%y-%m-%d %H:%M:%S ')
+   else:
+     ts=''
+   print "%s[%s] %s> %s" % (ts, severity, whoami(), mess)
+
+
+################################################################################
+# main
+################################################################################
+
+DEBUG=True
+TIMESTAMP=True
+
+if  __name__ == '__main__':
+   header()
+   banner("Autotest: banner()") 
+   message("Autotest: message()")
+   if DEBUG:
+      message("DEBUG is %s" % DEBUG, "debug")
+   else:
+      message("DEBUG is %s" % DEBUG)
+   message("TIMESTAMP is %s" % TIMESTAMP)
+   footer()
+
+
+################################################################################
+# eof
+################################################################################
---
+++ bin/pathUtils.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
++----------------------------------------------------------------------------+
+| pathUtils:  toolbox for pathname manipulation                              |
++---------+----------+----------+--------------------------------------------+
+| version | datecode | author   | history                                    |
++---------+----------+----------+--------------------------------------------+
+| 1.0     | 121116   | DD       | creation                                   |
+| 1.1     | 121205   | DD       | replace unique default argument            |
+|         |          |          | (filename='') by tuple (*args)             |
+|         |          |          | in currentPath class methods               |
+| 1.2     | 151215   | DD       | messagesUtils usage                        |
++---------+----------+----------+--------------------------------------------+
+'''
+
+VERSION = 'v1.1_121205'
+
+################################################################################
+# external modules
+################################################################################
+
+import os
+import sys
+
+
+################################################################################
+# local modules
+################################################################################
+
+from messagesUtils import header, banner, footer, message
+
+################################################################################
+# classes
+################################################################################
+
+class currentPath:
+   '''provide methods for getting project absolute pathnames.'''
+   def __init__(self):
+      self._bin = sys.path[0]
+      self._cfg = self._bin.replace('bin', 'cfg')
+      self._inp = self._bin.replace('bin', 'input')
+      self._out = self._bin.replace('bin', 'output')
+      self._tmp = self._bin.replace('bin', 'tmp')
+
+   def bin(self, *args):
+      '''returns current project 'bin' absolute path.'''
+      return os.path.join(self._bin, *args)
+      
+   def cfg(self, *args):
+      '''returns current project 'cfg' absolute path.'''
+      return os.path.join(self._cfg, *args)
+      
+   def inp(self, *args):
+      '''returns current project 'input' absolute path.'''
+      return os.path.join(self._inp, *args)
+      
+   def out(self, *args):
+      '''returns current project 'output' absolute path.'''
+      return os.path.join(self._out, *args)
+      
+   def tmp(self, *args):
+      '''returns current project 'tmp' absolute path.'''
+      return os.path.join(self._tmp, *args)
+      
+   def isFile(self, filepath):
+      '''returns true if file exists.'''
+      if os.path.isfile(filepath):
+         return True
+      else:
+         return False
+      
+   def isDir(self, dirpath):
+      '''returns true if dir exists.'''
+      if os.path.isdir(dirpath):
+         return True
+      else:
+         return False
+      
+
+################################################################################
+# main
+################################################################################
+
+if  __name__ == '__main__':
+   header()
+   banner("Autotest...")
+   cwd = currentPath()
+   config_file = cwd.cfg('default.cfg')
+   if cwd.isFile(config_file):
+      message("le fichier %s existe!" % config_file)
+   else:
+      message("le fichier %s n'existe pas!" % config_file, "error")
+   if cwd.isDir(cwd.inp()):
+      message("le repertoire %s existe!" % cwd.inp())
+   else:
+      message("le repertoire %s n'existe pas!" % cwd.inp(), "error")
+   output_file = cwd.out('result')
+   if not cwd.isFile(output_file):
+      message("le fichier %s n'existe pas!" % output_file, "error")
+   footer()
+
+
+################################################################################
+# eof
+################################################################################
