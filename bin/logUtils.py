31ebb55169d502102894da81b22abc8f19a12d9d
dd31530 <delannoy.daniel@free.fr>
2015-12-16 23:42:36 +0100

    change cfg/default.conf and bin/logUtils.py

--- bin/logUtils.py
+++ bin/logUtils.py
@@ -17,8 +17,10 @@ VERSION = 'v1.0_151216'
 # external modules
 ##############################################################################
 
+
 import logging
 from logging.handlers import RotatingFileHandler
+import sys
 
 
 ##############################################################################
@@ -55,19 +57,25 @@ CONFIG = config.read()
 logfile = cwd().log(CONFIG.get('log', 'LOG_FILE'))
 maxsize = CONFIG.get('log', 'MAX_SIZE')
 rot = CONFIG.get('log', 'ROT_NUMBER')
+loglev = CONFIG.get('log', 'LOGFILE_LVL', raw=True)
+logfmt = CONFIG.get('log', 'LOGFILE_FMT', raw=True)
+conlev = CONFIG.get('log', 'CONSOLE_LVL', raw=True)
+confmt = CONFIG.get('log', 'CONSOLE_FMT', raw=True)
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-file_formatter = logging.Formatter('%(asctime)s :: %(levelname)5s :: %(filename)s :: %(funcName)s :: %(message)s')
+file_formatter = logging.Formatter(logfmt)
 file_handler = RotatingFileHandler(logfile, 'a', maxsize, rot)
 file_handler.setLevel(logging.DEBUG)
+#file_handler.setLevel(loglev)
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
-console_formatter = logging.Formatter('[%(levelname)5s] %(funcName)s> %(message)s')
+console_formatter = logging.Formatter(confmt)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
+#console_handler.setLevel(conlev)
 console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
@@ -82,4 +90,3 @@ if __name__ == '__main__':
 ##############################################################################
 # eof
 ##############################################################################
-
